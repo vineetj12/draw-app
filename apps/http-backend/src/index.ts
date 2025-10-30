176ea99c48347ba28d3cee7470075b8063bcb60b
vineet <155095640+0123vineet@users.noreply.github.com>
2025-10-30 20:53:15 +0530

    first

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app=express();
+
+app.post('/Signup',()=>{
+    //check it in the db logic
+
+    
+})
+
+app.listen(3000,()=>{console.log("http-backend is up")}); 
